2fc461ec64cffd269b9ab58cc9793fe7d8345b9b
first <firstgang17@gmail.com>
2026-07-31 16:44:23 +0700

    update table

--- panaprisin-web/src/db/schema.ts
+++ panaprisin-web/src/db/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, varchar, integer, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTable, varchar, integer, timestamp, serial } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
-  id: uuid('id').defaultRandom().primaryKey(),
+  id: serial('id').primaryKey(),
   email: varchar('email', { length: 255 }).notNull().unique(),
   password: varchar('password', { length: 255 }).notNull(),
   role: varchar('role', { length: 50 }).default('user'),
@@ -9,9 +9,9 @@ export const users = pgTable('users', {
 });
 
 export const products = pgTable('products', {
-  id: uuid('id').defaultRandom().primaryKey(),
+  id: serial('id').primaryKey(),
   name: varchar('name', { length: 255 }).notNull(),
   price: integer('price').notNull(),
-  userId: uuid('user_id').references(() => users.id).notNull(), // ผูกกับตาราง users
+  userId: integer('user_id').references(() => users.id).notNull(),
   createdAt: timestamp('created_at').defaultNow(),
 });
